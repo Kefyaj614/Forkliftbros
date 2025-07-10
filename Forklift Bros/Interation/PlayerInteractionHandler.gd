e36ca0bcd4641c7d7c650314fc306a245ba36ed5
Kefyaj614 <128250560+Kefyaj614@users.noreply.github.com>
2025-07-10 01:32:32 -0400

    Add files via upload
    new update got some interaction code in the works

--- Forklift Bros/Interation/PlayerInteractionHandler.gd
+++ Forklift Bros/Interation/PlayerInteractionHandler.gd
@@ -1,6 +1,28 @@
-extends Node
+extends Node3D
+@export var ItemTypes : Array[ItemData] = []
+var Interactables : Array[InteractableItem] = []
+var Interacted : bool = false
+@onready var hand = $HandReach
 
-
+func SeeInteractable():
+	if hand.is_colliding():
+		print("I Can see This object and if it can be picked up it will be picked up")
+func OjbectInteraction(event: InputEvent):
+	if hand.is_colliding():
+		if (event.is_action_pressed("Interact")):
+			Interacted = true
+		
+	pass
+func PickupItem(event: InputEvent):
+	var Interactables : InteractableItem = null
+	if hand.is_colliding():
+		if(event.is_action_pressed("Interact")):
+			pass
+func ItemInteraction(event: InputEvent):
+	if hand.is_colliding:
+		if (event.is_action_pressed("Interact")):
+			print("Interacted")
+			pass
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,4 +30,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	SeeInteractable()
